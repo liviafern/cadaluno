195bddeba87f7115a21451e7972e11e560f65374
Fabio Lima <44278903+fabiomicromais@users.noreply.github.com>
2023-04-11 15:16:25 -0300

    pagina de contato

---
+++ pagina_contato.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<title><?php echo $titulo_site;?></title>
+<link href="estilo.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="site">
+<div id="cabecalho"><?php echo $titulo_site;?></div>
+  <div id="menu">
+  <ul> 
+   <?php echo $url_menus;?>
+  </ul>
+  </div>
+  <div id="conteudo"><h1>Contato</h1><p>Para entrar em contato, envie um email para: <a href="mailto:ivan@tocadigital.com.br">ivan@tocadigital.com.br</a></p></div>
+<div id="rodape"><?php echo $rodape;?></div>
+
+</div>
+
+</body>
+</html>
